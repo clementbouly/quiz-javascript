de4e99fc94d2c857535cdd18d575e93e2d4b9cfa
clementbouly <clementbouly@hotmail.fr>
2023-09-15 15:32:14 +0200

    feat(): Basic first implementation of quiz with react

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./App.css"
+import { QUESTIONS } from "./questions"
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [currentQuestionId, setCurrentQuestionId] = useState(0)
+	const [score, setScore] = useState(0)
+	const [displayResult, setDisplayResult] = useState(false)
+	const [selectedAnswer, setSelectedAnswer] = useState(null)
+	const currentQuestion = QUESTIONS[currentQuestionId]
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleSubmit = (event) => {
+		setSelectedAnswer(event.target.value)
+		handleAnswer(event.target.value)
+		setTimeout(() => {
+			handleNextQuestion()
+		}, 1000)
+	}
+
+	const handleAnswer = (answerSelected) => {
+		if (answerSelected == currentQuestion.correctAnswerId) {
+			setScore((prev) => prev + 1)
+		}
+	}
+
+	const handleNextQuestion = () => {
+		if (currentQuestionId !== QUESTIONS.length - 1) {
+			setCurrentQuestionId((prev) => prev + 1)
+		} else {
+			setDisplayResult(true)
+			setCurrentQuestionId(0)
+		}
+		setSelectedAnswer(null)
+	}
+
+	const handleRestart = () => {
+		setDisplayResult(false)
+		setScore(0)
+	}
+
+	const getAnswerClass = (answerId) => {
+		if (!selectedAnswer) return ""
+		
+		if (answerId == currentQuestion.correctAnswerId) {
+			return "correct"
+		} else if (answerId == selectedAnswer) {
+			return "incorrect"
+		}
+	}
+	
+
+
+	return (
+		<>
+			{displayResult ? (
+				<>
+					<h1>Result</h1>
+					<p>
+						You scored {score} out of {QUESTIONS.length}
+					</p>
+					<button onClick={handleRestart}>Restart</button>
+				</>
+			) : (
+				<>
+					<h1>{currentQuestion.question}</h1>
+					<ul className="answers">
+						{currentQuestion.answers.map((answer) => (
+							<li className="label" key={answer.id}>
+								<label className={getAnswerClass(answer.id)}>
+									{answer.text}
+									<input type="radio" id={answer.id} value={answer.id} onClick={handleSubmit} disabled={selectedAnswer} />
+								</label>
+							</li>
+						))}
+					</ul>
+					<p>
+						Score: {score} / {QUESTIONS.length}
+					</p>
+				</>
+			)}
+		</>
+	)
 }
 
 export default App
---
+++ react-quiz/src/questions.js
@@ -0,0 +1,60 @@
+// array with objects representing each question
+// each question has a question, 4 answers, and the correct answer
+
+export const QUESTIONS = [
+	{
+		id: "q11",
+		question: "What does CSS specificity determine when multiple rules apply to an element?",
+		answers: [
+			{ id: 1, text: "The order in which rules are applied" },
+			{ id: 2, text: "The importance of a rule" },
+			{ id: 3, text: "Which rule takes precedence" },
+			{ id: 4, text: "The color of the element" },
+		],
+		correctAnswerId: 3,
+	},
+	{
+		id: "q12",
+		question: "In React, what is the purpose of the virtual DOM?",
+		answers: [
+			{ id: 1, text: "To create a 3D user interface" },
+			{ id: 2, text: "To optimize memory usage" },
+			{ id: 3, text: "To improve rendering performance" },
+			{ id: 4, text: "To replace the actual DOM" },
+		],
+		correctAnswerId: 3,
+	},
+	{
+		id: "q13",
+		question: "Which HTML tag is used to embed a video?",
+		answers: [
+			{ id: 1, text: "<img>" },
+			{ id: 2, text: "<video>" },
+			{ id: 3, text: "<embed>" },
+			{ id: 4, text: "<iframe>" },
+		],
+		correctAnswerId: 2,
+	},
+	{
+		id: "q14",
+		question: "What is the purpose of the JavaScript `map()` function?",
+		answers: [
+			{ id: 1, text: "To filter elements in an array" },
+			{ id: 2, text: "To create a new array with modified elements" },
+			{ id: 3, text: "To remove elements from an array" },
+			{ id: 4, text: "To sort an array in ascending order" },
+		],
+		correctAnswerId: 2,
+	},
+	{
+		id: "q15",
+		question: "Which of the following is a valid way to declare a JavaScript variable?",
+		answers: [
+			{ id: 1, text: "const x = 10;" },
+			{ id: 2, text: "variable x = 10;" },
+			{ id: 3, text: "let x: 10;" },
+			{ id: 4, text: "x := 10;" },
+		],
+		correctAnswerId: 1,
+	},
+]
